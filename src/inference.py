74637d91155d4ffd6baf6b001802ff4b8bf01acb
KaushikNITK <148050332+KaushikNITK@users.noreply.github.com>
2024-12-08 12:56:47 +0530

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -76,38 +76,41 @@ def get_feed(username: str, category_id: int = Query(None), mood: str = Query(No
             final_rec = post_features_df.loc[post_features_df['category_id'] == category_id].iloc[top_k_indices.squeeze(0), :].index
         else:    
             raise HTTPException(status_code=404, detail="User is not in database so give input of mood and category_id")
-            user_features = user_features.drop(columns=['username', 'user_id']).values
-
-            # Handle mood embedding if mood is provided
+    else:
+        user_features = user_features.drop(columns=['username', 'user_id']).values
+        # Handle mood embedding if mood is provided
+        if mood:
+            mood_emb = bert_model(torch.tensor([tokenizer.encode(mood, add_special_tokens=True)])).last_hidden_state.squeeze(0)[0]
+            mood_emb = torch.tensor(mood_emb.detach().numpy(), dtype=torch.float32).unsqueeze(0)
+        
+        if category_id is not None:
+            # If mood is provided, filter posts based on both category and mood
             if mood:
-                mood_emb = bert_model(torch.tensor([tokenizer.encode(mood, add_special_tokens=True)])).last_hidden_state.squeeze(0)[0]
-                mood_emb = torch.tensor(mood_emb.detach().numpy(), dtype=torch.float32).unsqueeze(0)
-            
-            if category_id is not None:
-                # If mood is provided, filter posts based on both category and mood
-                if mood:
-                    category_posts_mood_emb = torch.tensor(post_features_df.loc[post_embed_df['category_id'] == category_id].iloc[:, -768:].values, dtype=torch.float32)
-                    scores = torch.matmul(mood_emb, category_posts_mood_emb.T)
-                    top_k = min(1000, scores.shape[1])
-                    top_k_indices = torch.topk(scores, k=top_k, dim=1).indices.cpu().numpy()
-                    category_posts = post_embed_df.loc[top_k_indices[0]].loc[post_embed_df['category_id'] == category_id]
-                    post_features = category_posts.drop(columns=['category_id', 'post_id']).values
-                    recommendations = get_recommendations(user_features, post_features, top_k)
-                    final_rec = post_features_df.loc[post_features_df['category_id'] == category_id].iloc[recommendations.squeeze(0), :].index
-                else:
-                    # If only category_id is provided, filter posts based on category
-                    category_posts = post_embed_df.loc[post_embed_df['category_id'] == category_id]
-                    if category_posts.empty:
-                        raise HTTPException(status_code=404, detail="Invalid category id")
-                    post_features = category_posts.drop(columns=['category_id', 'post_id']).values
-                    recommendations = get_recommendations(user_features, post_features, top_k)
-                    final_rec = post_features_df.loc[post_features_df['category_id'] == category_id].iloc[recommendations.squeeze(0), :].index
-            else:
-                # If no category_id is provided, use all posts
-                post_features = post_embed_df.drop(columns=['category_id', 'post_id']).values
+                category_posts_mood_emb = torch.tensor(post_features_df.loc[post_embed_df['category_id'] == category_id].iloc[:, -768:].values, dtype=torch.float32)
+                scores = torch.matmul(mood_emb, category_posts_mood_emb.T)
+                top_k = min(1000, scores.shape[1])
+                top_k_indices = torch.topk(scores, k=top_k, dim=1).indices.cpu().numpy()
+                category_posts = post_embed_df.loc[top_k_indices[0]].loc[post_embed_df['category_id'] == category_id]
+                post_features = category_posts.drop(columns=['category_id', 'post_id']).values
                 recommendations = get_recommendations(user_features, post_features, top_k)
-                final_rec = post_features_df.iloc[recommendations.squeeze(0), :].index
+                final_rec = post_features_df.loc[post_features_df['category_id'] == category_id].iloc[recommendations.squeeze(0), :].index
+            else:
+                # If only category_id is provided, filter posts based on category
+                category_posts = post_embed_df.loc[post_embed_df['category_id'] == category_id]
+                if category_posts.empty:
+                    raise HTTPException(status_code=404, detail="Invalid category id")
+                post_features = category_posts.drop(columns=['category_id', 'post_id']).values
+                recommendations = get_recommendations(user_features, post_features, top_k)
+                final_rec = post_features_df.loc[post_features_df['category_id'] == category_id].iloc[recommendations.squeeze(0), :].index
+        else:
+            # If no category_id is provided, use all posts
+            post_features = post_embed_df.drop(columns=['category_id', 'post_id']).values
+            recommendations = get_recommendations(user_features, post_features, top_k)
+            final_rec = post_features_df.iloc[recommendations.squeeze(0), :].index
 
     # Fetch recommended video links
     rec_video_links = all_posts_df.iloc[final_rec, :][["video_link", "id"]].to_dict(orient='records')
+    # while len(rec_video_links)<10:
+    #     idx = list(torch.randint(low=0, high=999, size=(1,)))
+    #     rec_video_links.append(all_posts_df["video_link"][idx].values[0])
     return {"username": username, "category_id": category_id, "mood": mood, "video_links": rec_video_links[0:10]}
