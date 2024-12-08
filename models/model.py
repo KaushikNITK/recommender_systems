ec903dc6e9c1ec718a9bc78ff974063d5ee00371
KaushikNITK <148050332+KaushikNITK@users.noreply.github.com>
2024-12-08 13:08:00 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
 
+# creating a class for two tower architecture
 class TwoTowerModel(nn.Module):
+    '''Two Tower Model takes in user features and gives out user embeddings'''
     def __init__(self, user_input_dim, post_emb, embedding_dim):
         super(TwoTowerModel, self).__init__()
         # User tower
@@ -13,10 +15,12 @@ class TwoTowerModel(nn.Module):
         )
 
     def forward(self, user_features):
+        '''this derives embeddings of user based on the parameters learnt form the training dataset'''
         user_embedding = self.user_tower(user_features)
         return user_embedding
 
     def predict(self, user_features, post_embeddings):
+        ''' this function outputs the dot product or cosine similarities of user features with post features'''
         user_embedding = self.forward(user_features)
         return torch.sum(user_embedding * post_embeddings, dim=-1)
     
